94041b9ef13f70b0c05256555b3c3ce0be02f94f
Anant3008 <sastrulaanant@gmail.com>
2025-01-18 15:25:58 +0530

    made changes according to review

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 
 app.get('/ping', (req, res) => {
-    res.send('Pong!');
+    try{
+        res. send('Pong!');
+    }catch(error){
+        next(error)
+    }
 });
 
 const PORT = process.env.PORT || 3000;
+if(!process.env.PORT) console.warn("PORT environment is not setUsing default port 3000")
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
